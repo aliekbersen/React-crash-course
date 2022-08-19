c2ed6851c7a3a7765302388d17d9ec29d0506391
RexenN <96340446+RexenN@users.noreply.github.com>
2022-08-19 15:56:12 +0300

    conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
-
+import { useState } from 'react';
 import './App.css';
 import Clock from './components/Clock';
+import Login from './components/Login';
 // import Welcome from './components/Welcome';
 
 function App() {
+  const [flag, setFlag] = useState(true)
   const userInfo = {
     firstname: 'Dipesh',
     lastname: 'Malvi'
   }
+  const toggle = () => {
+    setFlag(!flag);
+  }
   
   return (
-    <Clock />
+ <div>
+  <Login/>
+ </div>
+ 
+    //  <div> 
+  //   <button onClick={toggle}>Toggle Clock</button>
+  //   {flag ? <Clock /> : 'No clock  component'}
+  // </div>
   //  <div className="App">
   //     <Welcome user='Ali'/>
   //     <Welcome user='Ekber'/>
@@ -18,5 +30,6 @@ function App() {
   //   </div>
   );
 }
+
 
 export default App;
--- src/components/Clock.js
+++ src/components/Clock.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 
 function Clock() {
     const [time, setTime] = useState(new Date().toString())
-    const [flag, setFlag] = useState(true)
+    
 
     useEffect (() => {
         const interval =setInterval(showDate, 1000)
