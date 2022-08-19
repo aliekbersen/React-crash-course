3794ff2df158082049e8312cc9ab75605fff8d84
RexenN <96340446+RexenN@users.noreply.github.com>
2022-08-19 11:44:30 +0300

    components and props

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Welcome from './components/Welcome';
 
 function App() {
+  const userInfo = {
+    firstname: 'Dipesh',
+    lastname: 'Malvi'
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Welcome user='Ali'/>
+      <Welcome user='Ekber'/>
+      <Welcome user='Sen'/>
     </div>
   );
 }
---
+++ src/components/Welcome.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Welcome( props ) {
+  return (
+    <div>Hello, {props.user} from Welcome Component</div>
+  )
+}
+
+export default Welcome
